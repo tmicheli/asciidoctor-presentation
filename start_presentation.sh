4972f056c5393d200cb817067378f8308194fc6b
Tobias Michelis <tobias.michelis@redhat.com>
2020-02-26 12:20:38 +0100

    Some improvements
    * No working internet connection needed anymore
    * adoc file can be passed as an argument
    * the presentation is opened directly

--- start_presentation.sh
+++ start_presentation.sh
@@ -1,4 +1,35 @@
 #!/usr/bin/env bash
-podman run -e ADOC_FILE='presentation.adoc' -v $(pwd)/slides:/slides:Z -p 2342:2342 localhost/asciidoctor/revealjs 2>&1 >/dev/null &
-sleep 15
-xdg-open http://localhost:2342
+
+# default values
+adoc='presentation.adoc'
+
+if [ $# -ne 0 ]; then
+  while getopts "hf:" opt; do
+    case ${opt} in
+      h)
+        echo "This script converts yur adoc presentation to a revealjs"
+        echo "presentation and opens a tab in your favourite browser."
+        echo "Usage:"
+        echo ""
+        echo "$(basename $0) [-f FILENAME]"
+        echo ""
+        echo "were FILENAME is the name of your presentation without path"
+        echo "and is located in ./slides directory."
+        ;;
+      f)
+        adoc=${OPTARG}
+        ;;
+      ?)
+        echo "Usage: start_presentation.sh [-f FILENAME] "
+        exit 1
+        ;;
+    esac
+  done
+  shift $((OPTIND -1))
+fi
+# convert adoc to slides
+podman run -e ADOC_FILE="${adoc}" -v $(pwd)/slides:/slides:Z -p 2342:2342 localhost/asciidoctor/revealjs 2>&1 >/dev/null &
+# give the server some time to come up
+sleep 10
+# connect with your favourite browser
+xdg-open http://localhost:2342/"${adoc%.adoc}".html
